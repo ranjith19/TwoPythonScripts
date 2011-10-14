e675d7d8cdb3f5cf58a605b2a9b908aae35a706c
Ranjith Ramachandra <ranjith19@gmail.com>
2011-10-14 19:22:34 +0530

    successfully sent command and read the output. also a row of database is sent and received successfully

--- script_win/WindowsScript.py
+++ script_win/WindowsScript.py
@@ -2,7 +2,7 @@ import pyodbc #to load the ODBC module.
 import paramiko
 
 
-
+	
 #defining local database variables
 DB_SERVER='{SQL SERVER}' #name of server
 SERVER_LOC='localhost'#location of server
@@ -12,7 +12,7 @@ DB_NAME='omsdata'#name of database
 HOST='184.172.207.82'
 UNAME='pearlwhite85'
 PWD='*data85'
-
+#defining functions
 
 #establishing connections
 ssh = paramiko.SSHClient()
@@ -21,13 +21,15 @@ ssh.connect(HOST, username=UNAME, password=PWD)
 
 #conncting to locaal database.
 CONNECTION_STRING='DRIVER='+DB_SERVER+';SERVER='+SERVER_LOC+';DATABASE='+DB_NAME
-
 db_connection=pyodbc.connect(CONNECTION_STRING) #connection to database
-
 cursor=db_connection.cursor()#defining a cursor for connection
 
 cursor.execute("select * from dbo.inv_data")#Selecting all rows  ####needs work####
-row = 
-for row in curser:
-	s=print row
-	print s.type
+row=cursor.fetchone()
+text_row=''
+for i in range(0,len(row)):
+	text_row=text_row+str(row[i])+' '
+
+stdin, stdout, stderr = ssh.exec_command('python2.7 /home/pearlwhite85/test_proj/test.py '+text_row)
+print stdout.read()
+print stderr.read()
