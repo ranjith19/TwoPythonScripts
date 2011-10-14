52bf29276e06b43984b886ae6c3535fc1cb77071
Ranjith Ramachandra <ranjith19@gmail.com>
2011-10-14 16:56:35 +0530

    developed ssh connction

--- script_win/WindowsScript.py
+++ script_win/WindowsScript.py
@@ -1,13 +1,33 @@
-import pyodbc #to loda the ODBC module.
+import pyodbc #to load the ODBC module.
+import paramiko
 
 
-DB_SERVER='{SQL SERVER}'
-SERVER_LOC='localhost'
-DB_NAME='omsdata'
 
+#defining local database variables
+DB_SERVER='{SQL SERVER}' #name of server
+SERVER_LOC='localhost'#location of server
+DB_NAME='omsdata'#name of database
+
+#Defining ssh connection varaiables
+HOST='184.172.207.82'
+UNAME='pearlwhite85'
+PWD='*data85'
+
+
+#establishing connections
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(HOST, username=UNAME, password=PWD)
+
+#conncting to locaal database.
 CONNECTION_STRING='DRIVER='+DB_SERVER+';SERVER='+SERVER_LOC+';DATABASE='+DB_NAME
 
-db_connection=pyodbc.connect('DRIVER={SQL Server};SERVER=localhost;DATABASE=omsdata') #connection to database
-cursor=connection.cursor()#defining a cursor for connection
-cursor.execute("select * from dbo.inv_data")#Selecting all rows
+db_connection=pyodbc.connect(CONNECTION_STRING) #connection to database
 
+cursor=db_connection.cursor()#defining a cursor for connection
+
+cursor.execute("select * from dbo.inv_data")#Selecting all rows  ####needs work####
+row = 
+for row in curser:
+	s=print row
+	print s.type
