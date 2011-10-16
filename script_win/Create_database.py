2b87084c644930191acf82c9f319f70402ffca85
Ranjith Ramachandra <ranjith19@gmail.com>
2011-10-16 19:18:28 +0530

    tested set

--- script_win/Create_database.py
+++ script_win/Create_database.py
@@ -5,7 +5,7 @@ import connection_script
 
 def create_database():
 	connection, cursor= connection_script.connect_to_sqlite()
-	cursor.execute("create table inv_data (prod_cd char(21) not null , whs_num char(8) not null , UPDT_DT numeric(8,0), STATUS char(1) not null);")
+	cursor.execute("create table inv_data (prod_cd char(21) not null , whs_num char(8) not null, UPDT_DT numeric(8,0), STATUS char(1) not null);")
 	connection.commit()
 	cursor.close()
 	connection.close()
--- script_win/connection_script.py
+++ script_win/connection_script.py
@@ -8,9 +8,9 @@ import sys
 
 
 def connect_to_mssql():
-	CONNECTION_STRING='DRIVER='+settings_script.DB_SERVER+';SERVER='+settings_script.SERVER_LOC+';DATABASE='+settings_script.DB_NAME
-	db_connection=pyodbc.connect(CONNECTION_STRING) #connection to database
-	cursor=db_connection.cursor()#defining a cursor for connection
+	CONNECTION_STRING='DRIVER='+settings_script.MS_SERVER+';SERVER='+settings_script.MS_SERVER_LOC+';DATABASE='+settings_script.MS_DB_NAME
+	connection=pyodbc.connect(CONNECTION_STRING) #connection to database
+	cursor=connection.cursor()#defining a cursor for connection
 	return connection, cursor
 
 def connect_to_sqlite():
