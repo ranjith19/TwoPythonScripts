e8349549d35ff9d27b426e2bd58b32d73996c89f
Ranjith Ramachandra <ranjith19@gmail.com>
2011-10-19 01:20:01 +0530

    created midle database

--- script_win/db_create.py
+++ script_win/db_create.py
@@ -5,7 +5,8 @@ import connection_script
 
 def create_database():
 	connection, cursor= connection_script.connect_to_sqlite()
-	cursor.execute("create table inv_data (prod_cd char(21) not null , whs_num char(8) not null, UPDT_DT numeric(8,0), STATUS char(1) not null);")
+	cursor.execute("""DROP TABLE inv_data_bk;""")
+	cursor.execute()
 	connection.commit()
 	cursor.close()
 	connection.close()
@@ -16,3 +17,4 @@ except sqlite3.OperationalError:
 	print 'Error:table already exists',sys.exc_info()[0]
 except:
 	print 'Error:unexpected error',sys.exc_info()[0]
+	
